b2049626037af827b2563c9bc4cd2a764b476f8b
yutotanigawa <61495631+yutotanigawa@users.noreply.github.com>
2020-03-09 19:01:32 +0900

    [Add]投稿、編集機能

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,37 @@
 class BooksController < ApplicationController
+
   def index
+  	@book = Book.new
   end
 
   def show
+  	@book = Book.find(params[:id])
+
   end
 
-  def new
+  def create
+
+  	book = Book.new(book_params)
+  	if book.save
+  		flash[:notice] = "Book was successfully created."
+  	   redirect_to book_path(book.id)
+  	else
+  		render book_path
+  	end
   end
 
   def edit
+  	@book = Book.find(params[:id])
   end
+
+  def update
+  	book = Book.find(params[:id])
+  	book.update(book_params)
+  	redirect_to book_path(book)
+  end
+
+  def book_params
+  	params.require(:book).permit(:title, :body)
+  end
+
 end
